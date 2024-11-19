6272c1f5a177ddcda6884509902f4b23ede36726
farhan <farhanvirenze18@gmail.com>
2024-11-19 22:33:00 +0700

    bikin val di Mahasiswa

--- app/src/main/java/com/pam/quest6_139/ui/model/Mahasiswa.kt
+++ app/src/main/java/com/pam/quest6_139/ui/model/Mahasiswa.kt
@@ -1,3 +1,9 @@
 package com.pam.quest6_139.ui.model
 
-data class Mahasiswa()
+data class Mahasiswa(
+    val nim: String= "",
+    val nama: String= "",
+    val email: String= "",
+    val namaMatakuliah: String= "",
+    val kelas: String= "",
+)
