60225cf05096e0e8db2640d5e6014ceae2e5a526
farhan <farhanvirenze18@gmail.com>
2024-11-19 22:27:47 +0700

    commit Splash.kt

--- app/src/main/java/com/pam/quest6_139/ui/view/screen/Splash.kt
+++ app/src/main/java/com/pam/quest6_139/ui/view/screen/Splash.kt
@@ -1,2 +1,51 @@
 package com.pam.quest6_139.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.pam.quest6_139.R
+
+@Composable
+fun SplashView(
+    onMulaiButton: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.umy
+            ),
+            contentDescription = "umy",
+            modifier = Modifier.size(170.dp) // Ubah ukuran gambar di sini
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = { onMulaiButton() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp),
+        ) {
+            Text(text = "Login")
+        }
+    }
+}
