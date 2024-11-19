f84b8596fc3d9ddd62939151bda379e0be8c7881
farhan <farhanvirenze18@gmail.com>
2024-11-19 22:41:23 +0700

    commit MahasiswaViewModel.kt

--- app/src/main/java/com/pam/quest6_139/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/pam/quest6_139/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,33 @@
 package com.pam.quest6_139.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.pam.quest6_139.ui.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel: ViewModel(){
+
+    private val _statusUI = MutableStateFlow(Mahasiswa())
+
+    val statusUI: StateFlow<Mahasiswa> = _statusUI.asStateFlow()
+
+    fun setMahasiswa(ls: MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2]
+            )
+        }
+    }
+    fun setMatakuliah(ls: MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                namaMatakuliah = ls[0],
+                kelas = ls[1],
+            )
+        }
+    }
 }
