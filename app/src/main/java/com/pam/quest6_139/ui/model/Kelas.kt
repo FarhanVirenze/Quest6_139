ce3bd0d2e1df8750d8b13946c73046554eef54e7
farhan <farhanvirenze18@gmail.com>
2024-11-19 22:38:23 +0700

    bikin val listKelas di object Kelas

--- app/src/main/java/com/pam/quest6_139/ui/model/Kelas.kt
+++ app/src/main/java/com/pam/quest6_139/ui/model/Kelas.kt
@@ -1,4 +1,12 @@
 package com.pam.quest6_139.ui.model
 
+import com.pam.quest6_139.R
+
 object Kelas {
+    val listKelas = listOf(
+        R.string.A,
+        R.string.B,
+        R.string.C,
+        R.string.D,
+    )
 }
